83d67df2419b71e5853d6178eb2b7e6a592ec23b
sayhara <impact82@naver.com>
2022-01-17 18:42:04 +0900

    [CREATE] add social Login(Google)

--- src/main/java/com/qwon/eat_together/social/OAuthAttributes.java
+++ src/main/java/com/qwon/eat_together/social/OAuthAttributes.java
@@ -28,7 +28,7 @@ public class OAuthAttributes {
             return ofKakao("id",attributes);
         }
 
-        return ofNaver(userNameAttributeName,attributes);
+        return ofGoogle(userNameAttributeName,attributes);
 
     }
 
@@ -46,15 +46,12 @@ public class OAuthAttributes {
                 .build();
     }
 
-    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
-
-        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
-
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
-                .name((String) response.get("name"))
-                .email((String) response.get("email"))
-                .picture((String) response.get("profile_image"))
-                .attributes(response)
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
