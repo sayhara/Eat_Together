a8d93cc21815d2507d94ef8457838f871a1d4d2b
sayhara <impact82@naver.com>
2022-02-05 19:37:34 +0900

    [UPDATE] enable meeting function process

--- src/main/java/com/qwon/eat_together/domain/Meeting.java
+++ src/main/java/com/qwon/eat_together/domain/Meeting.java
@@ -1,50 +1,50 @@
-package com.qwon.eat_together.domain;
-
-import lombok.*;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
-
-@Entity
-@Getter @Setter @EqualsAndHashCode(of="id")
-@Builder @AllArgsConstructor @NoArgsConstructor
-
-public class Meeting {
-
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @ManyToOne
-    private Set<Account> manager=new HashSet<>();
-
-    @ManyToMany
-    private Set<Account> members=new HashSet<>();
-
-    @Column(unique = true)
-    private String url;
-
-    private String title;
-
-    private String short_message;
-
-    private String long_message;
-
-    private String image;
-
-    private LocalDateTime publishTime;
-
-    private LocalDateTime closedTime;
-
-    private LocalDateTime recruitTime;
-
-    private boolean recruiting;
-
-    private boolean published;
-
-    private boolean closed;
-
-    private boolean banner;
-
-}
+//package com.qwon.eat_together.domain;
+//
+//import lombok.*;
+//
+//import javax.persistence.*;
+//import java.time.LocalDateTime;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//@Entity
+//@Getter @Setter @EqualsAndHashCode(of="id")
+//@Builder @AllArgsConstructor @NoArgsConstructor
+//
+//public class Meeting {
+//
+//    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    @ManyToOne
+//    private Set<Account> manager=new HashSet<>();
+//
+//    @ManyToMany
+//    private Set<Account> members=new HashSet<>();
+//
+//    @Column(unique = true)
+//    private String url;
+//
+//    private String title;
+//
+//    private String short_message;
+//
+//    private String long_message;
+//
+//    private String image;
+//
+//    private LocalDateTime publishTime;
+//
+//    private LocalDateTime closedTime;
+//
+//    private LocalDateTime recruitTime;
+//
+//    private boolean recruiting;
+//
+//    private boolean published;
+//
+//    private boolean closed;
+//
+//    private boolean banner;
+//
+//}
