efceecdeaf0e4b029e6c3c5917cb97c83701a443
sayhara <impact82@naver.com>
2022-02-27 14:02:46 +0900

    [DELETE] delete social login

--- src/main/java/com/qwon/eat_together/social/User.java
+++ src/main/java/com/qwon/eat_together/social/User.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class User extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
